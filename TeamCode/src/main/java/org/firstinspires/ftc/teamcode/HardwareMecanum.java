7352a4b0441254f54177568577535634cc53bee7
Jestin VanScoyoc <coyotepod@gmail.com>
2022-05-26 12:10:39 -0400

    Working on the HardwareMap and HardwareMecanum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMecanum.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwareMecanum {
+
+    /* Public OpMode */
+    public DcMotor frontLeftDrive = null;
+    public DcMotor frontRightDrive = null;
+    public DcMotor backLeftDrive = null;
+    public DcMotor backRightDrive = null;
+
+    HardwareMap hwMap = null;
+    
+
+    }
+}
